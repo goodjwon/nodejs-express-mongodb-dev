726ca29732211e2b2946d40efa02c4398c698cc0
goodjwon <jwon76@naver.com>
2018-09-10 20:56:11 +0900

    - handlebar 추가 - index - about 탬플릿 추가

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 const express = require("express");
+const exphbs  = require('express-handlebars');
 
 const app = express();
 
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 const port = 5000;
+
+app.use(function(req, res, next){
+
+    console.log(Date.now());
+    req.name = "jwon good"
+    next();
+
+});
+
+//index Rout
+app.get('/', (req, res)=>{
+    console.log(req.name);
+    const title = 'Welcome1';
+    res.render('index', {
+        title: title
+    });
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about');
+})
+
 
 app.listen(port, function(){
     console.log(`Server started on port ${port}`);
