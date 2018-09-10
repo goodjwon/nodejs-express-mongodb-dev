e1060d463c1fd6bede7dddf578ec76c42ee1c76e
goodjwon <jwon76@naver.com>
2018-09-11 07:29:12 +0900

    ..

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.use(function(req, res, next){
+   console.log(Date.now());
+   req.name = 'Jwon Goods';
+   next();
+});
+
+app.get('/', function(req, res){
+    console.log(req.name);
+    res.send('INDX');
+});
+
+app.get('/about', function(req, res){
+    res.send('ABOUT');
+});
+
 const port = 5000;
 
 app.listen(port, function(){
