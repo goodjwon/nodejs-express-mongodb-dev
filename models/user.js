cfb9bd6c3c320acc34f1f1a104fef7b164ca0692
goodjwon <jwon76@naver.com>
2018-09-17 22:41:06 +0900

    회원가입 입력폼 유효성 검사 추가

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongooe =  require('mongoose');
+const Schema = mongooe.Schema;
+
+const UserSchema = new Schema({
+    name:{type:String, required:true},
+    email:{type:String, required:true},
+    password:{type:String, required:true},
+    date:{type:Date, default:Date.now}
+});
+
+mongooe.model('users', IdeaSchema);
