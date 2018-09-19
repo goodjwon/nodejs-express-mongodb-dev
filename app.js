7f94e16ee34d8bc903d4772431d3d2e259ae54b4
goodjwon <jwon76@naver.com>
2018-09-19 16:43:32 +0900

    하는데 잘 안되네..

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
-const express = require("express");
-const exphbs  = require('express-handlebars');
+const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
-const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const passport = require('passport');
+const mongoose = require('mongoose');
 
 const app = express();
 
-// Local routes
+// Load routes
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
 
 // Passport Config
 require('./config/passport')(passport);
 
-
-const port = 5000;
-
-mongoose.connect('mongodb://localhost/goodjwon',{
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+    useNewUrlParser: true
 })
-    .then(()=>console.log("MongoDB Connected..."))
-    .catch(err=>console.log(err));
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
-
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
-
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Static folder
@@ -42,40 +41,49 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Method override middleware
 app.use(methodOverride('_method'));
+
+// Express session midleware
 app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
-  }));
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
-//Global variables
+// Global variables
 app.use(function(req, res, next){
-    console.log(Date.now());
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
-})
+});
 
-//index Rout
-app.get('/', (req, res)=>{
-    console.log(req.name);
-    const title = 'Welcome1';
+// Index Route
+app.get('/', (req, res) => {
+    const title = 'Welcome';
     res.render('index', {
         title: title
     });
 });
 
-app.get('/about', (req, res)=>{
+// About Route
+app.get('/about', (req, res) => {
     res.render('about');
 });
+
 
 // Use routes
 app.use('/ideas', ideas);
 app.use('/users', users);
 
-// Run server
-app.listen(port, function(){
+const port = 5000;
+
+app.listen(port, () =>{
     console.log(`Server started on port ${port}`);
 });
